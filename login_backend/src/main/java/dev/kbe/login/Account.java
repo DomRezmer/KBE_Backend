96891c0dbfea5eb3404ea9fec7c572eac03ebd9f
DomRezmer <dom.rezmer@gmail.com>
2023-08-02 17:19:04 +0200

    pre-commit test

--- login_backend/src/main/java/dev/kbe/login/Account.java
+++ login_backend/src/main/java/dev/kbe/login/Account.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+//TESTING STUFF DELETE LATER
 @Document(collection = "accounts")
 @Data
 @AllArgsConstructor
