fe4687fa72672cb15251f4bcbec839492c6f9f87
DomRezmer <dom.rezmer@gmail.com>
2023-08-18 12:39:47 +0200

    fix: Cors Policy issue

--- login_backend/src/main/java/dev/kbe/login/AccountController.java
+++ login_backend/src/main/java/dev/kbe/login/AccountController.java
@@ -8,6 +8,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,23 +17,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:3000" }, allowedHeaders = { "*" })
 @RequestMapping("/api/v1/accounts")
 public class AccountController {
     @Autowired
     private AccountService accountService;
 
     @GetMapping
-    public ResponseEntity<List<Account>> getAllAccounts(){
+    public ResponseEntity<List<Account>> getAllAccounts() {
         return new ResponseEntity<List<Account>>(accountService.allAccounts(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Account>> getSingleAccount(@PathVariable ObjectId id){
+    public ResponseEntity<Optional<Account>> getSingleAccount(@PathVariable ObjectId id) {
         return new ResponseEntity<Optional<Account>>(accountService.singleAccount(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Account> createAccount(@RequestBody Map<String, String> payload){
-        return new ResponseEntity<Account>(accountService.createAccount(payload.get("email"), payload.get("name"), payload.get("dateofBirth"), payload.get("password")), HttpStatus.CREATED);
+    public ResponseEntity<Account> createAccount(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<Account>(accountService.createAccount(payload.get("email"), payload.get("name"),
+                payload.get("dateofBirth"), payload.get("password")), HttpStatus.CREATED);
     }
 }
--- login_backend/src/main/java/dev/kbe/login/AccountService.java
+++ login_backend/src/main/java/dev/kbe/login/AccountService.java
@@ -12,8 +12,6 @@ import org.springframework.stereotype.Service;
 public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
-    @Autowired
-    private MongoTemplate mongoTemplate;
 
     public List<Account> allAccounts() {
         return accountRepository.findAll();
