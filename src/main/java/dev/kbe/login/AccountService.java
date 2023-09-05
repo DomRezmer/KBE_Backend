9fce0bc87fd82953780021ea59baad116103d22a
DomRezmer <dom.rezmer@gmail.com>
2023-09-05 23:49:31 +0200

    Jenkins works!!

--- src/main/java/dev/kbe/login/AccountService.java
+++ src/main/java/dev/kbe/login/AccountService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
+//import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
